3375e63bee75cc3ef91b79aee62c68748ab563e3
Maxim Popelnitskiy <max077@mail.ru>
2014-11-18 16:10:19 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once('Application/Application.php');
+
+try {
+    Application::run();
+} catch (Exception $ex) {
+    if ($_SERVER['SERVER_NAME'] == 'localhost') {
+        echo $ex->getMessage() . "<br><br>Trace:<br>";
+        print_r($ex->getTrace());
+    } else {
+        //echo $ex->getMessage() . "\r\n\r\nSERVER:\r\n" . print_r($_SERVER, true) , "\r\nTrace:\r\n" . print_r($ex->getTrace(), true);
+        //exit;
+        mail('max077@mail.ru', "[{$_SERVER['SERVER_NAME']}] error", $ex->getMessage() . "\r\n\r\nSERVER:\r\n" . print_r($_SERVER, true) , "\r\nTrace:\r\n" . print_r($ex->getTrace(), true));
+    }
+}
